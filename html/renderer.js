6562724731913fee6c01a91d1de5b7926049c25f
wbazant <wbazant@ebi.ac.uk>
2016-12-12 11:49:12 +0000

    Bundles for the demo

---
+++ html/renderer.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+//*------------------------------------------------------------------*
+
+var AutocompleteBox = require('../src/AutocompleteBox.jsx');
+
+
+exports.render = function(options) {
+
+    ReactDOM.render(
+        React.createElement(
+            AutocompleteBox,
+            {
+              suggesterUrlTemplate: options.suggesterUrlTemplate || "https://www.ebi.ac.uk/gxa/json/suggestions?query={0}&species="
+            }
+        ),
+        (typeof options.target === "string") ? document.getElementById(options.target) : options.target
+    );
+};
